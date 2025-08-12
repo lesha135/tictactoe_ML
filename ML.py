3a53949b47f098c2757704de80bbbd9f7f566112
alekseisenkov <joschajoscha9@gmail.com>
2025-08-12 16:23:45 +0200

    remake of evolutin and fixing bags

--- ML.py
+++ ML.py
@@ -2,56 +2,60 @@ import random
 import copy
 
 
-class AI():
-    def __init__(self, input, neurons,layers, net=None):
+class AI:
+    def __init__(self, input, neurons, layers, net=None):
         self.input = input
         self.neurons = neurons
         self.layers = layers
         if net is None:
-            self.net = [[[[random.random() * 4 - 2, random.random() - 0.5] for _ in range(neurons)] for _ in range(neurons)] for _ in range(layers-1)]
-            self.net.insert(0,[[[random.random() * 4 - 2, random.random() - 0.5] for _ in range(input)] for _ in range(neurons)])
-            self.net.append([[[random.random() * 4 - 2, random.random() - 0.5] for _ in range(neurons)] for _ in range(input)])
+            self.net = [[[[self.get_random(), self.get_random()] for _ in range(neurons)] for _ in range(neurons)] for _ in range(layers - 1)]
+            self.net.insert(0, [[[self.get_random(), self.get_random()] for _ in range(input)] for _ in range(neurons)])
+            self.net.append([[[self.get_random(), self.get_random()] for _ in range(neurons)] for _ in range(input)])
         else:
             self.net = net
+
+    def get_random(self):
+        return random.random() * 4 - 2
+
     def play(self, input):
-        neuron_net = [[0]*self.neurons for _ in range(self.layers)]
-        neuron_net.insert(0,input)
-        neuron_net.append([0]*self.input)
+        neuron_net = [[0] * self.neurons for _ in range(self.layers)]
+        neuron_net.insert(0, input)
+        neuron_net.append([0] * self.input)
         for i in range(self.neurons):
             for j in range(self.input):
                 neuron_net[1][i] += neuron_net[0][j] * self.net[0][i][j][0] + self.net[0][i][j][1]
-        for l in range(1,self.layers):
+        for l in range(1, self.layers):
             for i in range(self.neurons):
                 for j in range(self.neurons):
-                    neuron_net[l+1][i] += neuron_net[l][j] * self.net[l][i][j][0] + self.net[l][i][j][1]
+                    neuron_net[l + 1][i]    += neuron_net[l][j] * self.net[l][i][j][0] + self.net[l][i][j][1]
         for i in range(self.input):
             for j in range(self.neurons):
                 neuron_net[-1][i] += neuron_net[-2][j] * self.net[-2][i][j][0] + self.net[-2][i][j][1]
         for i in range(len(neuron_net[-1])):
             if neuron_net[-1][i] < 0:
                 neuron_net[-1][i] = 0
-        d = sum(neuron_net[-1])
-        if d == 0:
-            return random.randint(0,self.input-1)
-        for i in range(self.input):
-            neuron_net[-1][i] = neuron_net[-1][i] / d
-        rand = random.random()
-        step = 0
-        for i in range(self.input):
-            if step <= rand <= step + neuron_net[2][i]:
-                return i
-            step += neuron_net[2][i]
-        return -1
+        return neuron_net[-1].index(max(neuron_net[-1]))
+        # d = sum(neuron_net[-1])
+        # if d == 0:
+        #     return random.randint(0, self.input - 1)
+        # for i in range(self.input):
+        #     neuron_net[-1][i] = neuron_net[-1][i] / d
+        # rand = random.random()
+        # step = 0
+        # for i in range(self.input):
+        #     if step <= rand <= step + neuron_net[-1][i]:
+        #         return i
+        #     step += neuron_net[-1][i]
+        # return -1
 
-    def evolve(self, copies_num,learning_rate):
-        ans = [AI(self.input,self.neurons,self.layers,self.net)]
-        for _ in range(copies_num-1):
-            net = copy.deepcopy(self.net)
-            for i in range(self.neurons):
-                for j in range(self.input):
-                    net[0][i][j][0] += (random.random()*2-1)*learning_rate
-                    net[1][j][i][0] += (random.random()*2-1)*learning_rate
-                    net[0][i][j][1] += (random.random()*2-1)*learning_rate/2
-                    net[1][j][i][1] += (random.random()*2-1)*learning_rate/2
-            ans.append(AI(self.input,self.neurons,self.layers,net))
-        return ans
+    def mutate(self, learning_rate, another_ai):
+        net = copy.deepcopy(self.net)
+        for i in range(self.layers):
+            for ii in range(len(net[i])):
+                for iii in range(len(net[i][ii])):
+                    for iv in range(len(net[i][ii][iii])):
+                        rand = 1 if random.random() > 0.5 else 0
+                        net[i][ii][iii][iv] = self.net[i][ii][iii][iv] * rand + another_ai.net[i][ii][iii][iv] * (1 - rand)
+                        net[i][ii][iii][iv] += (random.random() * 2 - 1) * learning_rate if random.random() < 0.2 else 0
+        return AI(self.input,self.neurons,self.layers,net=net)
+
--- tictactoe.py
+++ tictactoe.py
@@ -1,12 +1,13 @@
 import pygame
 import ML
+import random
 
 pygame.init()
 screen = pygame.display.set_mode((700, 700))
 clock = pygame.time.Clock()
 
 
-class TictactoeFild():
+class TictactoeField:
     def __init__(self, pos, size_pixel, size):
         self.size = size
         self.board = [[0] * size for _ in range(size)]
@@ -35,30 +36,32 @@ class TictactoeFild():
             ans = Ai.play(self.board_line)
         if self.board_line[ans] != 0:
             self.done = True
-            return (self.player % 2) + 1
+            return (self.player % 2) + 1, ans
         self.board[ans // self.size][ans % self.size] = self.player
         self.board_line[ans] = (self.player - 1.5) * 2
         self.player = (self.player % 2) + 1
+
         for x in range(self.size):
             for y in range(self.size):
+                if self.board[x][y] == 0:
+                    continue
+
                 if self.size - x >= 3:
-                    if self.board[x][y] == self.board[x + 1][y] == self.board[x + 2][y] and self.board[x][y] != 0:
+                    if self.board[x][y] == self.board[x + 1][y] == self.board[x + 2][y]:
                         self.done = True
-                        return self.board[x][y]
+                        return self.board[x][y], -1
                 if self.size - y >= 3:
-                    if self.board[x][y] == self.board[x][y + 1] == self.board[x][y + 2] and self.board[x][y] != 0:
+                    if self.board[x][y] == self.board[x][y + 1] == self.board[x][y + 2]:
                         self.done = True
-                        return self.board[x][y]
+                        return self.board[x][y], -1
                 if self.size - y >= 3 and self.size - x >= 3:
-                    if self.board[x][y] == self.board[x + 1][y + 1] == self.board[x + 2][y + 2] and self.board[x][
-                        y] != 0:
+                    if self.board[x][y] == self.board[x + 1][y + 1] == self.board[x + 2][y + 2]:
                         self.done = True
-                        return self.board[x][y]
+                        return self.board[x][y], -1
                 if self.size - y >= 3 and x >= 2:
-                    if self.board[x][y] == self.board[x - 1][y + 1] == self.board[x - 2][y + 2] and self.board[x][
-                        y] != 0:
+                    if self.board[x][y] == self.board[x - 1][y + 1] == self.board[x - 2][y + 2]:
                         self.done = True
-                        return self.board[x][y]
+                        return self.board[x][y], -1
 
     def click(self, x, y):
         if self.done:
@@ -110,31 +113,52 @@ class TictactoeFild():
                              (self.pos[0] + row * self.size_pixel / self.size, self.pos[1] + self.size_pixel))
 
 
+readInit = True
 evolve = False
-field = TictactoeFild((50, 50), 600, 3)
-num_of_ai = 200
-rounds = 11
-Ai=[]
-with open('/Users/alekseisenkov/PycharmProjects/tictactoe_ML/ML_saves.txt', 'r') as f:
-    for _ in range(4):
-        ai = f.readline()
-        net = ai.split(']]], [[[')
-        for i in range(len(net)):
-            net[i] = net[i].split(']], [[')
-        for i in range(len(net)):
-            for ii in range(len(net[i])):
-                net[i][ii] = net[i][ii].split("], [")
-        for i in range(len(net)):
-            for ii in range(len(net[i])):
-                for iii in range(len(net[i][ii])):
-                    net[i][ii][iii] = list(map(float,net[i][ii][iii].split(", ")))
-        Ai.append(ML.AI(9, 64, 3, net))
-Ai = (Ai[0].evolve(num_of_ai // 4, 0.3) + Ai[1].evolve(num_of_ai // 4, 0.3) + Ai[2].evolve(num_of_ai // 4, 0.3)
-      + Ai[3].evolve(num_of_ai // 4, 0.3))
+field = TictactoeField((50, 50), 600, 3)
+num_of_ai = 100
+rounds = 1
+learning_rate = 0.4
+layers = 2
+neurons = 54
+num_best = 15
+
+
+def new_generation(Ai, num_of_ai):
+    ans = list(Ai)
+    for i in range(len(Ai), num_of_ai):
+        j = random.randint(0, len(Ai) - 1)
+        k = random.randint(0, len(Ai) - 1)
+        ans.append(Ai[j].mutate(learning_rate, Ai[k]))
+    return ans
+
+
+if readInit:
+    Ai = []
+    with open('ML_saves.txt', 'r') as f:
+        for _ in range(4):
+            ai = f.readline()
+            net = ai.split(']]], [[[')
+            for i in range(len(net)):
+                net[i] = net[i].split(']], [[')
+            for i in range(len(net)):
+                for ii in range(len(net[i])):
+                    net[i][ii] = net[i][ii].split("], [")
+            for i in range(len(net)):
+                for ii in range(len(net[i])):
+                    for iii in range(len(net[i][ii])):
+                        net[i][ii][iii] = list(map(float, net[i][ii][iii].split(", ")))
+            Ai.append(ML.AI(9, neurons, layers, net))
+    Ai = new_generation(Ai, num_of_ai)
+else:
+    Ai = [ML.AI(9, neurons, layers) for _ in range(num_of_ai)]
+
+generation = 1
 while True:
-    with open('/Users/alekseisenkov/PycharmProjects/tictactoe_ML/ML_saves.txt', 'w') as f:
+    with open('ML_saves.txt', 'w') as f:
         for ai in Ai[:4]:
-            f.write(str(ai.net)[4:-4]+"\n")
+            f.write(str(ai.net)[4:-4] + "\n")
+
     screen.fill((255, 255, 255))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -156,36 +180,40 @@ while True:
             if event.key == pygame.K_e:
                 evolve = not evolve
     if evolve:
-        winns = [0] * num_of_ai
+        winns = [[i, 0, 0, 0] for i in range(num_of_ai)]
+        finished = 0
+        draws = 0
         for i in range(num_of_ai):
             for j in range(num_of_ai):
+                if i == j:
+                    continue
                 for _ in range(rounds):
                     ans = None
                     ai = [i, j]
-                    winner={i:0, j:0}
                     index = 0
-                    while ans is None:
-                        for event in pygame.event.get():
-                            if event.type == pygame.QUIT:
-                                pygame.quit()
-                            if event.type == pygame.KEYDOWN:
-                                if event.key == pygame.K_e:
-                                    evolve = not evolve
-                        ans = field.generate_move(Ai[ai[index]])
-                        index = (index + 1) % 2
                     field.restart()
-                    ans -= 1
-                    winner[ai[index]] += 1
-                if winner[i]>winner[j]:
-                    winns[i]+=1
-                else:
-                    winns[j]+=1
-        best = []
-        for _ in range(4):
-            best.append(winns.index(max(winns)))
-            winns.pop(best[-1])
-        Ai = (Ai[best[0]].evolve(num_of_ai // 4, 0.3) + Ai[best[1]].evolve(num_of_ai // 4, 0.3)
-              + Ai[best[2]].evolve(num_of_ai // 4, 0.3) + Ai[best[3]].evolve(num_of_ai // 4, 0.3))
-        print(best)
+                    while ans is None:
+                        ans = field.generate_move(Ai[ai[index % 2]])
+                        index += 1
+                    if index < 9:
+                        winns[ai[index % 2]][1] += 1
+                        if ans[1] != -1:
+                            winns[ai[(index + 1) % 2]][1] -= 2
+                            winns[ai[(index + 1) % 2]][3] -= 1
+                        else:
+                            winns[ai[(index + 1) % 2]][1] += 1
+                            winns[ai[(index + 1) % 2]][2] += 1
+                            finished += 1
+                    else:
+                        draws += 1
+
+        winns.sort(key=lambda x: -x[1])
+
+        best = winns[:num_best]
+        bestAi = [Ai[best[i][0]] for i in range(len(best))]
+        Ai = new_generation(bestAi, num_of_ai)
+        print(generation, best)
+        print("Finished:", finished, "Draws:", draws)
+        generation += 1
     field.draw()
     pygame.display.update()
